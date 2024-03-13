651b750b989349366bad3335e2b8cef7d24ebe38
Denis Kolesnikov <exdis@proton.me>
2024-03-13 11:39:52 +0100

    Under construction

--- src/Input.elm
+++ src/Input.elm
@@ -1,17 +1,30 @@
 module Input exposing (input)
 
-import Html exposing (Html, div, span, text, textarea)
+import Html exposing (Html, div, span, br, text, textarea)
 import Html.Attributes exposing (class, property)
+
+underConstruction : String
+underConstruction = """
+░█░█░█▀█░█▀▄░█▀▀░█▀▄░░░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▄░█░█░█▀▀░▀█▀░▀█▀░█▀█░█▀█
+░█░█░█░█░█░█░█▀▀░█▀▄░░░█░░░█░█░█░█░▀▀█░░█░░█▀▄░█░█░█░░░░█░░░█░░█░█░█░█
+░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░░░▀▀▀░▀▀▀░▀░▀░▀▀▀░░▀░░▀░▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀░▀
+"""
+
+underConstructionBanner : List (Html ())
+underConstructionBanner =
+  underConstruction
+    |> String.split "\n"
+    |> List.map (\item -> div [] [text item, br [] []])
 
 noBreakSpace : String
 noBreakSpace = "\u{00A0}~\u{00A0}"
 
 input : () -> Html ()
 input model =
-  div [ class "input" ]
-      [ textarea [] []
-      , div [ class "prompt" ]
-        [ span [ class "bg" ] [ text noBreakSpace ]
-        , text  "" ]
-        ]
+  div [] [
+    div [] underConstructionBanner,
+    div [ class "prompt" ]
+      [ span [ class "bg" ] [ text noBreakSpace ]
+      , text  "" ]
+    ]
 
