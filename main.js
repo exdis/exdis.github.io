fbba66d920c5dd586149337a5cd45d5ce450a491
Denis Kolesnikov <exdis@proton.me>
2024-03-13 12:22:19 +0100

    Moved font

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import "./style.css";
 import { Elm } from "./src/Main.elm";
 
 const root = document.querySelector("#app div");
-const app = Elm.Main.init({ node: root });
+Elm.Main.init({ node: root });
